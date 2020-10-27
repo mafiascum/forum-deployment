b3a4ebe47133d5a593a88b95e2e778c60b54a723
Charlie Ciccia <cciccia@gmail.com>
2020-10-27 00:24:12 -0500

    fix issue w/ web not coming up a second time because stock vhosts files were gone

--- web/scripts/ms_vhosts.sh
+++ web/scripts/ms_vhosts.sh
@@ -8,4 +8,10 @@ render-template "${BITNAMI_ROOT_DIR}/scripts/apache/bitnami-templates/forum.conf
 render-template "${BITNAMI_ROOT_DIR}/scripts/apache/bitnami-templates/www.conf.tpl" | sed '/^\s*$/d' > "${APACHE_VHOSTS_DIR}/www.conf"
 render-template "${BITNAMI_ROOT_DIR}/scripts/apache/bitnami-templates/wiki.conf.tpl" | sed '/^\s*$/d' > "${APACHE_VHOSTS_DIR}/wiki.conf"
 
+# remove stock files
 ensure_apache_app_configuration_not_exists phpbb
+
+# ensure they still exist as empty files or the setup script breaks
+# TODO - figure out something here that doesn't suck
+touch "${APACHE_VHOSTS_DIR}/phpbb-vhost.conf"
+touch "${APACHE_VHOSTS_DIR}/phpbb-https-vhost.conf"
