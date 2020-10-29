b957f1ca123d2bac244138d25c602dfadd129e8e
Charlie Ciccia <cciccia@gmail.com>
2020-10-28 22:08:22 -0500

    fix backup issues

--- database/scripts/ms_perform_backup.sh
+++ database/scripts/ms_perform_backup.sh
@@ -5,9 +5,9 @@ DB_TAR_FILE_NAME="mafiascum.backup.$MAFIASCUM_ENVIRONMENT.db.$DATE.7z"
 DB_LATEST_TAR_FILE_NAME="mafiascum.backup.$MAFIASCUM_ENVIRONMENT.db.latest.7z"
 
 cd /tmp
-mysqldump --lock-tables=false --routines --triggers --databases ms_phpbb3 ms_mediawiki > ms_phpbb3_and_ms_mediawiki.sql
+mysqldump -u${PHPBB_DATABASE_USER} -p${PHPBB_DATABASE_PASSWORD} --lock-tables=false --routines --triggers --databases ms_phpbb3 ms_mediawiki > ms_phpbb3_and_ms_mediawiki.sql
 7z -mx=9 -p${MAFIASCUM_BACKUP_PASSWORD} a "$DB_TAR_FILE_NAME" ms_phpbb3_and_ms_mediawiki.sql
 rm -f ms_phpbb3_and_ms_mediawiki.sql
-/usr/local/bin/aws s3 cp "$DB_TAR_FILE_NAME" "s3://$AWS_BACKUP_BUCKET/db-backups/$DB_TAR_FILE_NAME"
-/usr/local/bin/aws s3 cp "s3://$AWS_BACKUP_BUCKET/db-backups/$DB_TAR_FILE_NAME" "s3://$AWS_BACKUP_BUCKET/db-backups/$DB_LATEST_TAR_FILE_NAME"
+aws s3 cp "$DB_TAR_FILE_NAME" "s3://$AWS_BACKUP_BUCKET/db-backups/$DB_TAR_FILE_NAME"
+aws s3 cp "s3://$AWS_BACKUP_BUCKET/db-backups/$DB_TAR_FILE_NAME" "s3://$AWS_BACKUP_BUCKET/db-backups/$DB_LATEST_TAR_FILE_NAME"
 rm -f $DB_TAR_FILE_NAME
