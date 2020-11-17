c279a48c7ab603a515f6298126b5c5a800736b2d
Charlie Ciccia <cciccia@gmail.com>
2020-11-16 23:50:57 -0600

    change the impetus for running after migrations to be the temp user table

--- web/scripts/ms_post_migrations.sh
+++ web/scripts/ms_post_migrations.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # If this migration has already been run, do not run again
-# in this case, we check that the user_skype column has already been dropped
-SQL_EXISTS="show columns from phpbb_users like 'user_skype'"
+# in this case, we check that the temp_user_old_style table has already been dropped
+SQL_EXISTS="show tables like 'temp_user_old_style'"
 
 if [ ! "$(mysql -N -s -h"${PHPBB_DATABASE_HOST}" -u"${PHPBB_DATABASE_USER}" -p"${PHPBB_DATABASE_PASSWORD}" -e"$SQL_EXISTS" "${PHPBB_DATABASE_NAME}")" ]
 then
