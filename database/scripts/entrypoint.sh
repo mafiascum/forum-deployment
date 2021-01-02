fc42c6abe2d7a4193d3c8a721ec2ab2276b6e110
Charlie Ciccia <cciccia@gmail.com>
2021-01-02 15:21:53 -0600

    cron fixes for db side

--- database/scripts/entrypoint.sh
+++ database/scripts/entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# Start cron
+service cron start
 # populate the volume if needed
 /opt/mafiascum/scripts/ms_restore_from_backup_if_necessary.sh
 
--- database/scripts/ms_perform_backup.sh
+++ database/scripts/ms_perform_backup.sh
@@ -6,7 +6,7 @@ DB_LATEST_TAR_FILE_NAME="mafiascum.backup.$MAFIASCUM_ENVIRONMENT.db.latest.7z"
 
 cd /tmp
 mysqldump -u${PHPBB_DATABASE_USER} -p${PHPBB_DATABASE_PASSWORD} --lock-tables=false --routines --triggers --databases ms_phpbb3 ms_mediawiki > ms_phpbb3_and_ms_mediawiki.sql
-7z -mx=9 -p${MAFIASCUM_BACKUP_PASSWORD} a "$DB_TAR_FILE_NAME" ms_phpbb3_and_ms_mediawiki.sql
+7z -mx=9 -mmt2 -p${MAFIASCUM_BACKUP_PASSWORD} a "$DB_TAR_FILE_NAME" ms_phpbb3_and_ms_mediawiki.sql
 rm -f ms_phpbb3_and_ms_mediawiki.sql
 aws s3 cp "$DB_TAR_FILE_NAME" "s3://$AWS_BACKUP_BUCKET/db-backups/$DB_TAR_FILE_NAME"
 aws s3 cp "s3://$AWS_BACKUP_BUCKET/db-backups/$DB_TAR_FILE_NAME" "s3://$AWS_BACKUP_BUCKET/db-backups/$DB_LATEST_TAR_FILE_NAME"
