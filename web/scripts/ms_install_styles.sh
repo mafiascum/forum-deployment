08f3eaefbd91781285a3e729b522c926e31cfd41
Charlie Ciccia <charlie.ciccia@singlewire.com>
2022-10-13 20:21:30 -0500

    master -> main for styles

--- web/scripts/ms_install_styles.sh
+++ web/scripts/ms_install_styles.sh
@@ -10,7 +10,7 @@ do
     split_line=($line)
     [ -d /opt/bitnami/phpbb/styles/${split_line[0]} ] && rm -rf /opt/bitnami/phpbb/styles/${split_line[0]} 
     mkdir /opt/bitnami/phpbb/styles/${split_line[0]}
-    curl -o /tmp/${split_line[0]}-master.zip https://codeload.github.com/${split_line[1]}/zip/master
-    unzip /tmp/${split_line[0]}-master.zip
-    mv /tmp/${split_line[0]}-master/* /opt/bitnami/phpbb/styles/${split_line[0]}/
+    curl -o /tmp/${split_line[0]}-main.zip https://codeload.github.com/${split_line[1]}/zip/main
+    unzip /tmp/${split_line[0]}-main.zip
+    mv /tmp/${split_line[0]}-main/* /opt/bitnami/phpbb/styles/${split_line[0]}/
 done < <( jq -rc 'to_entries | .[] | .key + " " + .value| tostring' styles.json )
