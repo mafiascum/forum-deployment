05fd0ea90fa8d2d69c5615fb8a2621dc327d1a96
Charlie Ciccia <cciccia@gmail.com>
2022-11-05 13:43:59 -0500

    reparse privmsgs too

--- web/scripts/reparse_quotes.py
+++ web/scripts/reparse_quotes.py
@@ -1,16 +1,18 @@
+from cgitb import text
 import mariadb
 import xml.etree.ElementTree as ET
 import sys
 import os
 import re
 import logging
+from functools import partial
 
-def reparse_quotes(cursor, limit, start=0):
+def reparse_quotes(table, id_column, text_column, necessary_insert_cols, cursor, limit, start=0):
 
     # get all posts within 'limit' starting with 'start' that match a particular pattern where a url to another post is embedded
-    query = ("select post_id, post_text, ExtractValue(post_text, '//QUOTE[@url]/s') as ref_quotes from phpbb_posts where post_text rlike '\\\\[quote=\"In \\\\[url=https?:\\/\\/(www\\.|forum\\.)?mafiascum\\.net(\\/forum)?\\/viewtopic\\.php\\\\?p=(\\\\d+)#.*?](.*?)\\\\[\\/url\\](.*?)\"]' and post_id > %s order by post_id limit %s")
+    query = f"select {id_column}, {text_column}, ExtractValue({text_column}, '//QUOTE[@url]/s') as ref_quotes from {table} where {text_column} rlike '\\\\[quote=\"In \\\\[url=https?:\\/\\/(www\\.|forum\\.)?mafiascum\\.net(\\/forum)?\\/viewtopic\\.php\\\\?p=(\\\\d+)#.*?](.*?)\\\\[\\/url\\](.*?)\"]' and {id_column} > %s order by {id_column} limit %s"
 
-    logging.debug(query)
+    logging.debug(f"select query: {query}")
     cursor.execute(query, (start, limit))
 
     all_ref_post_ids = set()
@@ -27,17 +29,17 @@ def reparse_quotes(cursor, limit, start=0):
                 all_ref_post_ids.add(ref_post_id)
                 ref_quotes_to_post_ids[ref_quote] = ref_post_id
         except:
-            logging.warning(f'Could not parse post content from quote: {ref_post_id}, {ref_quote}. Skipping.')
+            logging.warning(f'Could not parse post content from quote: {table}, {post_id}, {ref_quote}. Skipping.')
                 
         all_postdata[post_id] = post_text
 
     if not all_postdata:
-        logging.info("Found no posts. Finishing.")
+        logging.info(f"Found no posts for {table}. Finishing.")
         return None
 
-    # obtain information about posts referenced by posts under modification
+    # obtain information about posts referenced by posts/PMs under modification
     cursor.executemany("INSERT INTO t_ref_posts (post_id) VALUES (%s)", [(x, ) for x in all_ref_post_ids])
-    cursor.execute("select p.post_id, p.poster_id, p.post_time, u.username from phpbb_posts p join t_ref_posts r on p.post_id = r.post_id join phpbb_users u on p.poster_id = u.user_id")
+    cursor.execute(f"select p.post_id, p.poster_id, p.post_time, u.username from phpbb_posts p join t_ref_posts r on p.post_id = r.post_id join phpbb_users u on p.poster_id = u.user_id")
 
     all_mapped_posts = dict()
     # build a map of post_id -> info about that post to replace into quotes
@@ -67,21 +69,41 @@ def reparse_quotes(cursor, limit, start=0):
                     else:
                         logging.info(f'post {ref_post_id} was deleted, so we cannot link to it.')
                 except:
-                    logging.error(f'Something went wrong: {post_id}, {post_text}, {ref_post_id}')
+                    logging.error(f'Something went wrong: {table}, {post_id}, {post_text}, {ref_post_id}')
                     raise
         # hack to deal with the xml parser changing all brs to have a space before self close, which bbcode will totally ignore!
         to_update.append((post_id, re.sub(r'<br />', '<br/>', ET.tostring(root, 'unicode'))))
 
-    logging.debug(to_update)
+    logging.debug(f"replaced XML: {to_update}")
+
+    # insert is being used as a vehicle for doing bulk updates; we never actually insert
+    # but data integrity constraints will still fire if it thinks you're trying to insert a null val into a not null column
+    extra_coldefs = ''.join([f", {x}" for x in necessary_insert_cols])
+    extra_vals = ''.join([", 'blah'" for _x in necessary_insert_cols])
 
     # upsert changes
-    cursor.executemany("INSERT INTO phpbb_posts (post_id, post_text) VALUES (%s,%s) ON DUPLICATE KEY UPDATE post_text = %s",
-        [(post_id, post_text, post_text) for (post_id, post_text) in to_update])
+    try:
+        query = f"INSERT INTO {table} ({id_column}, {text_column}{extra_coldefs}) VALUES (%s,%s{extra_vals}) ON DUPLICATE KEY UPDATE {text_column} = %s"
+        logging.debug(f"update query: {query}")
+        cursor.executemany(query, [(post_id, post_text, post_text) for (post_id, post_text) in to_update])
+    except:
+        logging.error(f'Problem doing write replacement: {table}, {post_id}, {post_text}')
+        raise
 
     # find the max row we dealt with
     highest_post_id = max([x for x in all_postdata.keys()])
-    logging.info(f"Processed up to {highest_post_id}")
+    logging.info(f"Processed up to {highest_post_id} for {table}")
     return highest_post_id
+
+def reparse_entity(table, id_column, text_column, necessary_insert_cols, cursor):
+    logging.info(f'Starting {table} at 0.')
+    cursor.execute("TRUNCATE TABLE t_ref_posts")
+    reparse = partial(reparse_quotes, table, id_column, text_column, necessary_insert_cols)
+    highest_post_id = reparse(cursor, 1000)
+    while (highest_post_id):
+        cursor.execute("TRUNCATE TABLE t_ref_posts")
+        highest_post_id = reparse(cursor, 1000, highest_post_id)
+
 
 def main():
     cnx = mariadb.connect(
@@ -92,14 +114,13 @@ def main():
         autocommit=True
     )
     logging.basicConfig(stream=sys.stdout, level=os.getenv('LOG_LEVEL', logging.INFO), format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-    logging.info('Connection established. Starting at 0.')
+    logging.info('Connection established.')
     cursor = cnx.cursor()
     cursor.execute("CREATE TEMPORARY TABLE IF NOT EXISTS t_ref_posts (post_id integer primary key)")
 
-    highest_post_id = reparse_quotes(cursor, 1000)
-    while (highest_post_id):
-        cursor.execute("TRUNCATE TABLE t_ref_posts")
-        highest_post_id = reparse_quotes(cursor, 1000, highest_post_id)
+    reparse_entity("phpbb_privmsgs", "msg_id", "message_text", ["to_address", "bcc_address"], cursor)
+    reparse_entity("phpbb_posts", "post_id", "post_text", [], cursor)
+
 
 if __name__ == '__main__':
     main()
