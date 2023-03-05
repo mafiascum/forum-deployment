b92222a0a0eec771599d717a6d79b4ccdb359e32
Charlie Ciccia <charlie.ciccia@singlewire.com>
2023-03-05 12:19:41 -0600

    adjust ini modes for prod and local dev

--- web/scripts/ms_env_php.sh
+++ web/scripts/ms_env_php.sh
@@ -1,2 +1,10 @@
-ENV_TYPE="development"
+#!/bin/bash
+
+if [[ -n ${PHP_INI_DEVELOPMENT+development} ]]; then
+    echo "Using development ini"
+    ENV_TYPE="development"
+else
+    echo "Using production ini"
+    ENV_TYPE="production"
+fi
 mv "$PHP_INI_DIR/php.ini-$ENV_TYPE" "$PHP_INI_DIR/php.ini"
