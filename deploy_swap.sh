cf09d2da0b9ad3c950d2a68c591cc92432c81d16
Charlie Ciccia <charlie.ciccia@singlewire.com>
2023-05-09 20:05:20 -0500

    swap nginx out before we kill old containers duh

--- deploy_swap.sh
+++ deploy_swap.sh
@@ -2,7 +2,7 @@
 
 set -x
 
-swap_containers() {
+swap() {
     is_to_alternate=$1
     set -e
 
@@ -19,25 +19,21 @@ swap_containers() {
     docker-compose build "$new_container_dc"
     docker-compose up -d "$new_container_dc"
 
+    echo "Waiting until new container has come up"
     until [[ $(docker-compose exec $new_container_dc sh -c 'curl --max-time 2 -o /dev/null -s -w "%{http_code}\n" http://127.0.0.1:8080' | tr -d '\r') == "200" ]]; do sleep 5; done
-
-    docker-compose stop "$old_container_dc"
-    docker-compose rm -f "$old_container_dc"
-
-    set +e
-}
-
-toggle_upstreams() {
-    is_to_alternate=$1
-    set -e
 
     if [[ $is_to_alternate == true ]]; then
         docker-compose exec nginx sh -c 'cp /etc/nginx/nginx-green.conf /etc/nginx/nginx.conf'
     else
         docker-compose exec nginx sh -c 'cp /etc/nginx/nginx-blue.conf /etc/nginx/nginx.conf'
     fi
-
     docker-compose exec nginx sh -c "nginx -s reload"
+
+    echo "Waiting until old connections have been served before killing old containers"
+    sleep 60;
+
+    docker-compose stop "$old_container_dc"
+    docker-compose rm -f "$old_container_dc"
 
     set +e
 }
@@ -48,13 +44,11 @@ green_response=$(docker-compose exec web-alt sh -c 'curl --max-time 2 -o /dev/nu
 if [[ "$blue_response" == "200" && "$green_response" != "200" ]]; then
     echo "Blue (main) container is serving connections."
     echo "Green (alternate) container coming up to replace it."
-    swap_containers true
-    toggle_upstreams true
+    swap true
 elif [[ "$blue_response" != "200" && "$green_response" == "200" ]]; then
     echo "Green (alternate) container is serving connections."
     echo "Blue (main) container coming up to replace it."
-    swap_containers false
-    toggle_upstreams false
+    swap false
 elif [[ "$blue_response" == "200" && "$green_response" == "200" ]]; then
     echo "Both containers currently up. Unsure what to do. Kill one of them to continue."
     exit 1
