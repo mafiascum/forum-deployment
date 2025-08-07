b07ac6e27e9a2ef7095db4b6020bef82956f0d7c
Charlie Ciccia <charlie.ciccia@singlewire.com>
2025-08-07 15:18:35 -0500

    deploy swap should use 'docker compose'

--- deploy_swap.sh
+++ deploy_swap.sh
@@ -16,30 +16,30 @@ swap() {
         new_container_ip="172.40.0.11"
     fi
 
-    docker-compose build "$new_container_dc"
-    docker-compose up -d "$new_container_dc"
+    docker compose build "$new_container_dc"
+    docker compose up -d "$new_container_dc"
 
     echo "Waiting until new container has come up"
-    until [[ $(docker-compose exec $new_container_dc sh -c 'curl --max-time 2 -o /dev/null -s -w "%{http_code}\n" http://127.0.0.1:8080' | tr -d '\r') == "200" ]]; do sleep 5; done
+    until [[ $(docker compose exec $new_container_dc sh -c 'curl --max-time 2 -o /dev/null -s -w "%{http_code}\n" http://127.0.0.1:8080' | tr -d '\r') == "200" ]]; do sleep 5; done
 
     if [[ $is_to_alternate == true ]]; then
-        docker-compose exec nginx sh -c 'cp /etc/nginx/nginx-green.conf /etc/nginx/nginx.conf'
+        docker compose exec nginx sh -c 'cp /etc/nginx/nginx-green.conf /etc/nginx/nginx.conf'
     else
-        docker-compose exec nginx sh -c 'cp /etc/nginx/nginx-blue.conf /etc/nginx/nginx.conf'
+        docker compose exec nginx sh -c 'cp /etc/nginx/nginx-blue.conf /etc/nginx/nginx.conf'
     fi
-    docker-compose exec nginx sh -c "nginx -s reload"
+    docker compose exec nginx sh -c "nginx -s reload"
 
     echo "Waiting until old connections have been served before killing old containers"
     sleep 60;
 
-    docker-compose stop "$old_container_dc"
-    docker-compose rm -f "$old_container_dc"
+    docker compose stop "$old_container_dc"
+    docker compose rm -f "$old_container_dc"
 
     set +e
 }
 
-blue_response=$(docker-compose exec web sh -c 'curl --max-time 2 -o /dev/null -s -w "%{http_code}\n" http://127.0.0.1:8080' | tr -d '\r')
-green_response=$(docker-compose exec web-alt sh -c 'curl --max-time 2 -o /dev/null -s -w "%{http_code}\n" http://127.0.0.1:8080' | tr -d '\r')
+blue_response=$(docker compose exec web sh -c 'curl --max-time 2 -o /dev/null -s -w "%{http_code}\n" http://127.0.0.1:8080' | tr -d '\r')
+green_response=$(docker compose exec web-alt sh -c 'curl --max-time 2 -o /dev/null -s -w "%{http_code}\n" http://127.0.0.1:8080' | tr -d '\r')
 
 if [[ "$blue_response" == "200" && "$green_response" != "200" ]]; then
     echo "Blue (main) container is serving connections."
