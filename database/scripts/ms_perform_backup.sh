89c9a41b32fef09f3110f1eca060552c6ab9fdcf
Charlie Ciccia <charlie.ciccia@singlewire.com>
2025-09-24 09:27:10 -0500

    make backups completely nonblocking

--- database/scripts/ms_perform_backup.sh
+++ database/scripts/ms_perform_backup.sh
@@ -7,7 +7,7 @@ if [[ $MAFIASCUM_ENVIRONMENT != 'development' ]] && [[ $MAFIASCUM_ENVIRONMENT !=
     DB_LATEST_TAR_FILE_NAME="mafiascum.backup.$MAFIASCUM_ENVIRONMENT.db.latest.7z"
 
     cd /tmp
-    mysqldump -u${PHPBB_DATABASE_USER} -p${PHPBB_DATABASE_PASSWORD} --lock-tables=false --routines --triggers --databases ms_phpbb3 ms_mediawiki > ms_phpbb3_and_ms_mediawiki.sql
+    mysqldump -u${PHPBB_DATABASE_USER} -p${PHPBB_DATABASE_PASSWORD} --skip-lock-tables --quick --single-transaction --routines --triggers --databases ms_phpbb3 ms_mediawiki > ms_phpbb3_and_ms_mediawiki.sql
     7z -mx=9 -mmt2 -p${MAFIASCUM_BACKUP_PASSWORD} a "$DB_TAR_FILE_NAME" ms_phpbb3_and_ms_mediawiki.sql
     rm -f ms_phpbb3_and_ms_mediawiki.sql
     aws s3 cp "$DB_TAR_FILE_NAME" "s3://$AWS_BACKUP_BUCKET/db-backups/$DB_TAR_FILE_NAME"
