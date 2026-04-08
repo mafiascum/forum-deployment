0e0a63b69fe0ab86470273794c65d300d2c26993
Jack Nash <jacknash42@gmail.com>
2026-04-09 02:41:36 +1000

    add cache clearing script (to avoid tedium)

--- reset-cache.sh
+++ reset-cache.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker exec forum-deployment-web-1 sh -c "rm -f /data/forum/cache/production/url_matcher.php /data/forum/cache/production/url_matcher.php.meta && apache2ctl graceful"
+docker compose exec web sh -c "rm -f /data/forum/cache/production/url_matcher.php /data/forum/cache/production/url_matcher.php.meta && apache2ctl graceful"
