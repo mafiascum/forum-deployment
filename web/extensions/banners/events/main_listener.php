060a4a2969f8f7b8274448bd5886f157b2f934ce
Jack Nash <jacknash42@gmail.com>
2026-07-05 18:38:19 +1000

    basic extension setup

---
+++ web/extensions/banners/events/main_listener.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace mafiascum\banners\event;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+class main_listener implements EventSubscriberInterface {
+  public static function getSubscribedEvents() {
+    return array();
+  }
+}
