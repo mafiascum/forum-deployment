bc71c4a764ca844c16ee91934f11bebb66bb579d
Jack Nash <jacknash42@gmail.com>
2026-07-09 17:13:21 +1000

    remove 'how to join first game' link

--- web/www/index.php
+++ web/www/index.php
@@ -40,8 +40,8 @@
 				<div id='nav_container'>
 					<a class='nav' href='<?php echo(getenv('PHPBB_FORUM_SERVER_PROTOCOL') . getenv('FORUM_FQDN')) ?>' alt='Play here' title='Play here!'>Forum</a>&bull;
 					<a class='nav' href='<?php echo(getenv('PHPBB_FORUM_SERVER_PROTOCOL') . getenv('WIKI_FQDN')) ?>' alt='Learn here' title='Learn here!'>Wiki</a>&bull;
-					<a class='nav' href='<?php echo(getenv('PHPBB_FORUM_SERVER_PROTOCOL') . getenv('WIKI_FQDN')) ?>/index.php?title=Newbie_Guide' alt='Newbie Guide' title='Newbie Guide'>Info for new players</a>&bull;<br/>
-					<a class='nav' href='<?php echo(getenv('PHPBB_FORUM_SERVER_PROTOCOL') . getenv('WWW_FQDN')) ?>/howtojoin.svg' target='_blank' alt='Join your first game' title='Join your first game' target='_blank'>How to join your first game</a>
+					<a class='nav' href='<?php echo(getenv('PHPBB_FORUM_SERVER_PROTOCOL') . getenv('WIKI_FQDN')) ?>/index.php?title=Newbie_Guide' alt='Newbie Guide' title='Newbie Guide'>Info for new players</a>&bull;<!--<br/>
+					<a class='nav' href='<?php echo(getenv('PHPBB_FORUM_SERVER_PROTOCOL') . getenv('WWW_FQDN')) ?>/howtojoin.svg' target='_blank' alt='Join your first game' title='Join your first game' target='_blank'>How to join your first game</a>-->
 				<div>
 				<div class='social-icons'>
 					<a href='https://www.facebook.com/mafiascum.net/' target='_blank' alt='Official Facebook Page'><i class="fa fa-facebook-square"></i></a>
